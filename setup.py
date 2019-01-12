3c0aa858979db4cbe1f4430c14694ed9dd532c29
Grant Jenks <contact@grantjenks.com>
2019-01-11 21:08:39 -0800

    Refactor for CI and better docs

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-import patternmatching
-from setuptools import setup, find_packages
+from setuptools import setup
 from setuptools.command.test import test as TestCommand
-import sys
+
+import patternmatching
 
 
 class Tox(TestCommand):
@@ -9,43 +9,39 @@ class Tox(TestCommand):
         TestCommand.finalize_options(self)
         self.test_args = []
         self.test_suite = True
+
     def run_tests(self):
         import tox
         errno = tox.cmdline(self.test_args)
-        sys.exit(errno)
+        exit(errno)
 
 
-with open('README.rst') as fptr:
-    readme = fptr.read()
+with open('README.rst') as reader:
+    readme = reader.read()
 
 setup(
-    name='patternmatching',
+    name=patternmatching.__title__,
     version=patternmatching.__version__,
-    description='Python pattern matching library.',
+    description='Python Pattern Matching',
     long_description=readme,
     author='Grant Jenks',
     author_email='contact@grantjenks.com',
     url='http://www.grantjenks.com/docs/patternmatching/',
-    packages=find_packages(exclude=('tests', 'docs')),
-    package_data={'': ['LICENSE', 'README.rst']},
+    license='Apache 2.0',
+    packages=['patternmatching'],
     tests_require=['tox'],
     cmdclass={'test': Tox},
-    license='Apache 2.0',
-    install_requires=[],
     classifiers=(
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Natural Language :: English',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
     ),
